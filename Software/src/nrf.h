ce6dac80b05f362749a48b594ecf1066361b57d8
LeoDJ <leandro.spaeth@gmail.com>
2020-03-06 01:44:57 +0100

    establish NRF data protocol untested

---
+++ Software/src/nrf.h
@@ -0,0 +1,35 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <IPAddress.h>
+
+#define MAX_PACKET_LEN  1500
+
+typedef enum : uint8_t {
+    CMD_INIT = 0,
+    CMD_DATA = 1
+} cmd_t;
+
+#define INIT_HEADER_LEN     9
+#define INIT_PAYLOAD_LEN    (32 - INIT_HEADER_LEN)
+typedef struct {
+    cmd_t cmd;
+    uint32_t ip;
+    uint16_t port;
+    uint8_t checksum;
+    uint16_t len;
+    uint8_t* payload;
+} initPacket_t;
+
+#define DATA_HEADER_LEN     3
+#define DATA_PAYLOAD_LEN    (32 - DATA_HEADER_LEN)
+typedef struct {
+    cmd_t cmd;
+    uint8_t packetNum;
+    uint8_t checksum;
+    uint8_t* payload;
+} dataPacket_t;
+
+void initNRF();
+void loopNRF();
+void sendUDP(IPAddress ip, uint16_t port, String payload);
+void sendUDP(IPAddress ip, uint16_t port, uint8_t* payload, uint16_t size);
