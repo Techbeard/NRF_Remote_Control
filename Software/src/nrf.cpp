9afc73fa21e3dffe5d1cf1824639644b34fbc787
LeoDJ <leandro.spaeth@gmail.com>
2021-02-09 06:00:25 +0100

    add additional safety checks to prevent freezing and a bit more debug

--- Software/src/nrf.cpp
+++ Software/src/nrf.cpp
@@ -14,39 +14,56 @@ RF24Network network(radio);
 RF24Mesh mesh(radio, network);
 RF24EthernetClass RF24Ethernet(radio, network, mesh);
 
-RF24UDP udpClient; // Set UIP_CONF_UDP to 1 in uip-conf.h
+RF24UDP udpClient; // Set UIP_CONF_UDP to 1 in RF24Ethernet/uip-conf.h
 
 
 void nrfInit() {
     DEBUG.println("Radio Init...");
     Ethernet.begin(IPAddress(10,10,2,4));
-    bool success = mesh.begin(97, RF24_250KBPS);
-    if(!success) {
-        DEBUG.println("*ERR Mesh connection failed!");
-    }
+    bool success = false;
+    // while(!success) {
+        success = mesh.begin(97, RF24_250KBPS);
+        if(!success) {
+            DEBUG.println("*ERR Mesh connection failed!");
+        }
+    // }
     Ethernet.set_gateway(IPAddress(10,10,2,2));
     // radio.printDetails();
     DEBUG.print("NRF Power Level: ");
-    DEBUG.println(String(radio.getPALevel())); // 0-3 = MIN, LOW, HIGH, MAX
+    DEBUG.print(String(radio.getPALevel())); // 0-3 = MIN, LOW, HIGH, MAX
+    DEBUG.print(" / CH: x");
+    DEBUG.println(String(radio.getChannel(), 16));
     DEBUG.println("Radio Init done!");
+    radio.printDetails();
 }
 
 uint32_t meshTimer;
 
 void nrfLoop() {
     // Every 30 seconds, test mesh connectivity
-    if(millis() - meshTimer > 30000) { 
+    if(millis() - meshTimer > 5000) { 
         meshTimer = millis();
         if (!mesh.checkConnection() ) {
-            mesh.renewAddress();
+            uint16_t addr = mesh.renewAddress(100); // 100ms timeout for mesh renewal (original: 7500)
+            if(!addr) {
+                DEBUG.println("Mesh connection attempt failed");
+            }
         }
     }
 
 }
 void nrfSendUDP(IPAddress ip, uint16_t port, uint8_t* payload, uint16_t size) {
-    udpClient.beginPacket(ip, port);
-    udpClient.write(payload, size);
-    udpClient.endPacket();
+    // DEBUG.print("Snd... ");
+    // note: not a real indicator of success when mesh connection not established
+    int success = udpClient.beginPacket(ip, port); 
+    if(success) {
+        udpClient.write(payload, size);
+        udpClient.endPacket();
+        // DEBUG.println("Success");
+    }
+    else {
+        // DEBUG.println("Error sending UDP packet");
+    }
 }
 
 void nrfSendUDP(IPAddress ip, uint16_t port, String payload) {
